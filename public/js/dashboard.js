74fc8323918e6e22f42c40af27381699eab8acef
matertu <souzatertu321@gmail.com>
2026-05-24 20:12:39 -0300

    adicao front end 'html css e js' no diretorio public

---
+++ public/js/dashboard.js
@@ -0,0 +1,25 @@
+function atualizarContadorBoletins() {
+  const totalBoletins = document.querySelectorAll(".boletim-card").length;
+  document.getElementById("contadorBoletins").innerText = totalBoletins;
+}
+
+function visualizarArquivados() {
+  /* ALTERA O CONTADOR */ document.querySelector(
+    ".contador-boletins h2",
+  ).innerHTML = ` Boletins arquivados: <span id="contadorBoletins"> 5 </span> `;
+  /* ALTERA GRID */ const boletinsGrid =
+    document.querySelector(".boletins-grid");
+  boletinsGrid.innerHTML = ` <div class="boletim-card"> <h3>Boletim Arquivado 01</h3> <p>Data: 10/03/2026</p> <p>Boletim armazenado no sistema.</p> <span class="status"> ARQUIVADO </span> </div> <div class="boletim-card"> <h3>Boletim Arquivado 02</h3> <p>Data: 11/03/2026</p> <p>Boletim armazenado no sistema.</p> <span class="status"> ARQUIVADO </span> </div> <div class="boletim-card"> <h3>Boletim Arquivado 03</h3> <p>Data: 12/03/2026</p> <p>Boletim armazenado no sistema.</p> <span class="status"> ARQUIVADO </span> </div> <div class="boletim-card"> <h3>Boletim Arquivado 04</h3> <p>Data: 13/03/2026</p> <p>Boletim armazenado no sistema.</p> <span class="status"> ARQUIVADO </span> </div> <div class="boletim-card"> <h3>Boletim Arquivado 05</h3> <p>Data: 14/03/2026</p> <p>Boletim armazenado no sistema.</p> <span class="status"> ARQUIVADO </span> </div> `;
+  /* TROCA O BOTÃO */ document.querySelector(
+    ".arquivados-container",
+  ).innerHTML =
+    ` <button class="arquivados-btn" onclick="voltarBoletins()" > ← VOLTAR PARA BOLETINS DIGITALIZADOS </button> `;
+}
+
+function voltarBoletins() {
+  location.reload();
+}
+
+window.onload = function () {
+  atualizarContadorBoletins();
+};

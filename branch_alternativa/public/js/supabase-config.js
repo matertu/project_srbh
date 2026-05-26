16cc5139d38320f4ea7af931de8c161c26b1b705
matertu <souzatertu321@gmail.com>
2026-05-26 17:00:41 -0300

    feat: removidos arquivos .java

--- branch_alternativa/public/js/supabase-config.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = "https://ytrowyxkuemlqmiyfvll.supabase.co";
-
-const SUPABASE_ANON_KEY = "sb_publishable_IayDBaQKREcn9UabWjWC5Q_jvU8Ys7Z";
-
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
